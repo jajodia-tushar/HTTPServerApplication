84b0a3efa06e46997d2229f36172ee851dadadc6
Jajodia <tjajodia@internal.tavisca.com>
2019-08-06 10:45:31 +0530

    added Logger

--- src/main/java/com/tavisca/chatapplication/Server.java
+++ src/main/java/com/tavisca/chatapplication/Server.java
@@ -3,17 +3,24 @@ package com.tavisca.chatapplication;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.*;
+import java.util.logging.FileHandler;
 
 public class Server {
+    static Logger logger;
+    FileHandler fileHandler;
         private ServerSocket serverSocket;
         public Server(int port) throws IOException{
             this.serverSocket = new ServerSocket(port);
-            System.out.println("Server Started at Port "+port);
+            logger = Logger.getLogger(Server.class.getName());
+            logger.info("Server has been started at port --->"+port);
+            fileHandler = new FileHandler("default.log",true);
         }
 
         public ClientWrapper listen() throws IOException {
             Socket socket = this.serverSocket.accept();
-            System.out.println("New Client Accepted");
+            logger.addHandler(fileHandler);
+            logger.info("A new Client has been Accepted");
             return new ClientWrapper(socket);
         }
 
